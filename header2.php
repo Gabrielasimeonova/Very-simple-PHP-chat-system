944323a609d2ce29ab59796fe5bfc0b298061791
Gabriela Simeonova <gabrielasimeonova74@gmail.com>
2023-02-19 19:18:37 +0200

    Add files via upload

---
+++ header2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <style>
+body {
+ background-image: url("https://rare-gallery.com/uploads/posts/4562144-simple-simple-background-primary-colors-minimalism-geometry-abstract.png");
+ background-color: #cccccc;
+ height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+    a:hover{
+        color: black;
+    }
+    </style>
+</head>
+<body>
+        <li><a href="logout.php"></span> Logout</a></li>
+      </body>
+  
